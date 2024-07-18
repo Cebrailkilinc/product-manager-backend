bc023cbc817c75d6ec83c62a0f169dd4d5dbf46f
Cebrail KILINÇ <87018423+Cebrailkilinc@users.noreply.github.com>
2024-07-18 14:55:49 +0300

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
-import productRoutes from "./controller/productController.js"
+import productRoutes from "./controller/productController.js";
 import Product from "./model/productModel.js";
 
 // Environment variables
@@ -17,24 +17,29 @@ const port = process.env.PORT || 8080;
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
-app.use(express.json())
-app.use(express.static('public'))
-
+app.use(express.json());
+app.use(express.static('public'));
 
 // MongoDB connection
-connectDB();
-
+connectDB().then(() => {
+  console.log("Connected to MongoDB");
+}).catch((err) => {
+  console.error("Failed to connect to MongoDB", err);
+});
 
 app.get("/", async (req, res) => {
   try {
+    console.log("Received request at /");
     const products = await Product.find(); // Asenkron sorgu
+    console.log("Fetched products from DB");
     res.status(200).json(products);
   } catch (err) {
+    console.error("Error fetching products", err);
     res.status(500).json({ message: err.message });
   }
 });
 
-app.use("/", productRoutes)
+app.use("/product", productRoutes);
 
 // Start the server
 app.listen(port, () => {
