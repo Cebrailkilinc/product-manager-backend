bd225ae80fc4822ce7a400a2552fd5890539fb3d
Cebrail KILINÇ <87018423+Cebrailkilinc@users.noreply.github.com>
2024-07-18 10:47:59 +0300

    updaeta

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 app.use(express.json())
-
+app.use(express.static('public'))
 
 
 // MongoDB connection
