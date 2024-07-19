66178e69b315009a728d8152e96d8f75356a9cdd
Cebrail KILINÇ <87018423+Cebrailkilinc@users.noreply.github.com>
2024-07-19 11:41:09 +0300

    update index.js

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ import dotenv from "dotenv";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import { connectDB } from "./config/db.js";
-import productRoutes from "./controller/productController.js";
-import Product from "./model/productModel.js";
+import { connectDB } from "./config/db.js"
+import productRoutes from "./controller/productController.js"
 
 // Environment variables
 dotenv.config();
@@ -17,29 +16,19 @@ const port = process.env.PORT || 8080;
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
-app.use(express.json());
-app.use(express.static('public'));
+app.use(express.json())
+app.use(express.static('public'))
+
 
 // MongoDB connection
-connectDB().then(() => {
-  console.log("Connected to MongoDB");
-}).catch((err) => {
-  console.error("Failed to connect to MongoDB", err);
-});
+connectDB();
 
-app.get("/", async (req, res) => {
-  try {
-    console.log("Received request at /");
-    const products = await Product.find(); // Asenkron sorgu
-    console.log("Fetched products from DB");
-    res.status(200).json(products);
-  } catch (err) {
-    console.error("Error fetching products", err);
-    res.status(500).json({ message: err.message });
-  }
-});
 
-app.use("/product", productRoutes);
+app.get("/", (res,req)=>{
+  res.status(200).json("products");
+})
+
+app.use("/product", productRoutes)
 
 // Start the server
 app.listen(port, () => {
